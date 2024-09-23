0a0de6817843baabdd5f7e617d068c3ed46a5650
SALASFR\salak401 <danivive16@gmail.com>
2024-09-23 14:45:14 -0500

    hi

---
+++ PrivateBlog/PrivateBlog.Web/CustomConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PrivateBlog.Web.Data;
+
+namespace PrivateBlog.Web
+{
+    public static class CustomConfiguration
+    {
+        public static WebApplicationBuilder AddCustomBuilderConfiguration(this WebApplicationBuilder builder)
+        {
+            //Data Context
+            builder.Services.AddDbContext<DataContext>(Configuration =>
+            {
+                Configuration.UseSqlServer(builder.Configuration.GetConnectionString("MyConnection"));
+            });
+            return builder;
+        }
+    }
+}
---
+++ PrivateBlog/PrivateBlog.Web/Data/Entities/Section.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PrivateBlog.Web.Data.Entities
+{
+    public class Section
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Display(Name = "Sección")]
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+        public string Name { get; set; }
+
+        [Display(Name = "Descripción")]
+        public string? Description { get; set; }
+
+        [Display(Name = "¿Esta Oculta?")]
+        public bool IsHidden { get; set; }
+
+    }
+}
